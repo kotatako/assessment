337aeb106f0e89ea609aa61fec6e670c94a4f2bf
kotatako <65843964+kotatako@users.noreply.github.com>
2020-07-27 18:07:06 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -5,11 +5,11 @@ const resultDivided = document.getElementById('result-area');
 const tweetDivided = document.getElementById('tweet-area');
 
 /**
- * 指定した要素の子どもを全て除去する
+ * 指定した要素の子どもを全て削除する
  * @param {HTMLElement} element HTMLの要素
  */
 function removeAllChildren(element) {
-  while (element.firstChild) { // 子どもの要素があるかぎり除去
+  while (element.firstChild) { // 子どもの要素があるかぎり削除
     element.removeChild(element.firstChild);
   }
 }
@@ -19,6 +19,8 @@ assessmentButton.onclick = () => {
   if (userName.length === 0) { // 名前が空の時は処理を終了する
     return;
   }
+
+  
 
   // 診断結果表示エリアの作成
   removeAllChildren(resultDivided);
@@ -31,26 +33,34 @@ assessmentButton.onclick = () => {
   paragraph.innerText = result;
   resultDivided.appendChild(paragraph);
 
-  // ツイートエリアの作成
-  removeAllChildren(tweetDivided);
-  const anchor = document.createElement('a');
-  const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag='
-    + encodeURIComponent('あなたのいいところ')
-    + '&ref_src=twsrc%5Etfw';
-  anchor.setAttribute('href', hrefValue);
-  anchor.className = 'twitter-hashtag-button';
-  anchor.setAttribute('data-text', result);
-  anchor.innerText = 'Tweet #あなたのいいところ';
-  tweetDivided.appendChild(anchor);
+// TODO ツイートエリアの作成
+removeAllChildren(tweetDivided);
 
-  // widgets.js の設定
-  const script = document.createElement('script');
-  script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
-  tweetDivided.appendChild(script);
+const anchor = document.createElement('a');
+const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag='
+ + encodeURIComponent('あなたのいいところ') + 
+ '&ref_src=twsrc%5Etfw';
+
+
+anchor.setAttribute('href', hrefValue);
+anchor.className = 'twitter-hashtag-button';
+anchor.setAttribute('date-text', result);
+anchor.innerText = 'Tweet #あなたのいいところ';
+
+tweetDivided.appendChild(anchor);
+twttr.widgets.load();
+
+};
+
+userNameInput.onkeydown = (event) => {
+  if (event.key === 'Enter') {
+    // TODO ボタンのonclick() 処理を呼び出す
+    assessmentButton.onclick();
+  }
 };
 
 const answers = [
-  '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
+  '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
   '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
   '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
@@ -65,8 +75,7 @@ const answers = [
   '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
   '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
   '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
-  '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
-  '{userName}のいいところは優しさです。あなたの優しい雰囲気や立ち振る舞いに多くの人が癒やされています。'
+  '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
 ];
 
 /**
@@ -76,16 +85,16 @@ const answers = [
  */
 function assessment(userName) {
   // 全文字のコード番号を取得してそれを足し合わせる
-  let sumOfcharCode = 0;
+  let sumOfCharCode = 0;
   for (let i = 0; i < userName.length; i++) {
-    sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
+    sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
   }
 
   // 文字のコード番号の合計を回答の数で割って添字の数値を求める
-  const index = sumOfcharCode % answers.length;
+  const index = sumOfCharCode % answers.length;
   let result = answers[index];
 
-  result = result.replace(/{userName}/g, userName);
+  result = result.replace(/\{userName\}/g, userName);
   return result;
 }
 
